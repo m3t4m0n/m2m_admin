61b1770b5a87186003739fa52f721e6dc3ea7638
양효영 <>
2018-02-06 03:03:52 +0900

    fix routes

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,37 @@
+class UserController < ApplicationController
+  def signup
+    @signup = Mobile.new
+    signup_item = request.raw_post
+    data_JSON = JSON.parse(signup_item)
+
+    @signup.name = data_JSON['name']
+    @signup.password = data_JSON['password']
+    @signup.gender = data_JSON['gender']
+    @signup.type = data_JSON['type']
+    @signup.phone = data_JSON['phone']
+    # @info.token = data_JSON['token']
+
+    @signup.save
+
+    @status = "complete"
+    render json: @status
+
+  end
+
+  def signin
+    signin_item = request.raw_post
+    data_JSON = JSON.parse(signin_item)
+
+    @signin_name = Mobile.exists?(["name like ?", "#{data_JSON['name']}"])
+    @signin_password = Mobile.exists?(["password like ?", "#{data_JSON['password']}"])
+
+    if @signin_name && @signin_password
+      @check_flag = true
+    else
+      @check_flag = false
+    end
+
+    render json: @check_flag
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'user/signup'
+
+  get 'user/signin'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -6,9 +10,12 @@ Rails.application.routes.draw do
   get 'home/index'
 
   get '/car_call/status'
+  post '/car_call/status'
 
   post '/register/index'
-  get '/register/index'
+
+  post '/user/signup'
+  post '/user/signin'
 
   post '/car_call/pushMessage'
 
