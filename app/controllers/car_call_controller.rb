839261d9ba6a493369ae59fc4c9a60190d21d151
양효영 <>
2018-02-14 23:51:07 +0900

    add request_info

--- app/controllers/car_call_controller.rb
+++ app/controllers/car_call_controller.rb
@@ -24,23 +24,35 @@ class CarCallController < ApplicationController
     # }
 
     # response = fcm.send(registration_ids, options)
-    
+
     response = fcm.send_to_topic("car_call", data: { message: "This is a FCM car_call Message!" })
-                
+
     @flag = response
     render json: response
 
   end
-  
+
   def carReservRequest
     request_item = request.raw_post
     data_JSON = JSON.parse(request_item)
-    
-    request_info = Mobile.where("name = :name", :name => data_JSON['name'])
-    
-    
-    
-    
+
+    # 차량호출을 예약 신청한 사람의 정보 -> user_id를 얻어오기 위함
+    request_user = Mobile.where("name = :name", :name => data_JSON['name'])
+
+    request_info = Carreservrequest.new
+    request_info.location = data_JSON['location']
+    request_info.date = data_JSON['date']
+    request_info.time = data_JSON['time']
+    request_info.memo = data_JSON['memo']
+    request_info.wheel = data_JSON['wheel']
+    request_info.friend = data_JSON['friend']
+    request_info.mobile_id = request_user[0].id
+
+    render json: request_info.save
+
+
+
+
   end
 
 end
